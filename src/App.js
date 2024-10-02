0863d2cf7796fb783bcacf0c51004a6c68f55bc1
Kay R <kaynu521@gmail.com>
2024-10-02 10:40:55 +0900

    처음

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: 'red' }}>안녕하세요</h1>
+      <p className="pStyle">잘 지내시죠?</p>
+      <button>버튼</button>
     </div>
   );
 }
